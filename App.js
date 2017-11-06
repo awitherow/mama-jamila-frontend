fe9af6f8c8adcfd2a659823781d019e6a61242c9
awitherow <au.witherow@gmail.com>
2017-11-06 22:57:00 +0100

    add fonts

--- App.js
+++ App.js
@@ -8,22 +8,44 @@ import {
   Dimensions
 } from "react-native";
 
+import { Font } from "expo";
+
 export default class App extends React.Component {
+  state = {
+    loading: true
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      light: require("./src/assets/fonts/PFDinTextAR-Light.ttf"),
+      regular: require("./src/assets/fonts/PFDinTextAR-Regular.ttf"),
+      medium: require("./src/assets/fonts/PFDinTextAR-Medium.ttf")
+    });
+
+    this.setState({ loading: false });
+
+    // this.refs.body.fadeIn();
+  }
+
   render() {
     return (
       <Image
         style={styles.backgroundImage}
         source={require("./src/assets/intro-bg.jpg")}
       >
-        <View style={styles.titleContainer}>
-          <View style={styles.titleBackground}>
-            <Text style={styles.title}>Mama Jamila</Text>
-            <Text style={styles.subline}>A cute little subline</Text>
+        {this.state.loading ? null : (
+          <View style={styles.container}>
+            <View style={styles.titleContainer}>
+              <View style={styles.titleBackground}>
+                <Text style={styles.title}>Mama Jamila</Text>
+                <Text style={styles.subline}>A cute little subline</Text>
+              </View>
+            </View>
+            <TouchableHighlight style={styles.goButton}>
+              <Text>Let's go!</Text>
+            </TouchableHighlight>
           </View>
-        </View>
-        <TouchableHighlight style={styles.goButton}>
-          <Text>Let's go!</Text>
-        </TouchableHighlight>
+        )}
       </Image>
     );
   }
@@ -37,6 +59,9 @@ const styles = StyleSheet.create({
     alignSelf: "stretch",
     width: null
   },
+  container: {
+    flex: 1
+  },
   titleContainer: {
     flex: 1,
     justifyContent: "flex-end",
@@ -45,16 +70,18 @@ const styles = StyleSheet.create({
   },
   titleBackground: {
     backgroundColor: "rgba(242, 240, 221, 0.8)",
-    paddingVertical: 12,
-    paddingHorizontal: 24
+    paddingVertical: 8,
+    paddingHorizontal: 16
   },
   title: {
     fontSize: 32,
-    textAlign: "right"
+    textAlign: "right",
+    fontFamily: "medium"
   },
   subline: {
     fontSize: 16,
-    textAlign: "right"
+    textAlign: "right",
+    fontFamily: "light"
   },
   goButton: {
     width,
